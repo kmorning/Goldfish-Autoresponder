dd84fb582b2da624d7beb2dec9e9abacae4de0fa
Kelly Morning <kleemorning@gmail.com>
2021-06-26 19:33:33 +0100

    remove NOT NULL constraint from to_date

--- pg/create-table-autoresponder.sql
+++ pg/create-table-autoresponder.sql
@@ -2,7 +2,7 @@ CREATE TABLE autoresponder (
     email varchar(255) PRIMARY KEY, 
     descname varchar(255) default NULL,
     from_date date NOT NULL default '19990108',
-    to_date date NOT NULL default '19990108',
+    to_date date,
     message text NOT NULL,
     enabled boolean NOT NULL default false,
     subject varchar(255) NOT NULL default '',
